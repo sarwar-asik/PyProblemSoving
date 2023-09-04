f7f8f199351a4e77af61bc145d70cf7efefeede1
sarwar-asik <sarwarasik@gmail.com>
2023-09-04 16:18:32 +0600

    generate prime number

---
+++ generatePrime.py
@@ -0,0 +1,16 @@
+# 16. Python program to generate the prime numbers from 1 to N
+
+num = int(input("Enter the range of number :::   "))
+# here if num =10
+
+for n in range(2, num):
+    # here n = 3,4,5,6,7,8,9
+    for i in range(2, n):
+
+        # print('this is i', i)
+        if n % i == 0:
+            break
+        else:
+            print(n)
+
+
