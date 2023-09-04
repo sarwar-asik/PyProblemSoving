24376adf89cbdfdbb3435b9a11538d0e04e61e0a
sarwar-asik <sarwarasik@gmail.com>
2023-09-04 16:22:29 +0600

    generate prime number fiexed

--- generatePrime.py
+++ generatePrime.py
@@ -4,13 +4,21 @@ num = int(input("Enter the range of number :::   "))
 # here if num =10
 
 for n in range(2, num):
-    # here n = 3,4,5,6,7,8,9
+    is_prime = True
     for i in range(2, n):
-
-        # print('this is i', i)
-        if n % i == 0:
+        if (n % i) == 0:
+            is_prime = False
             break
-        else:
-            print(n)
+    if is_prime:
+        print(n)
+
+# for n in range(2, num):
+#     # here n = 3,4,5,6,7,8,9
+#     for i in range(2, n):
+#         # print('this is i', i)
+#         if (n % i) == 0:
+#             break
+#         else:
+#             print(n)
 
 
